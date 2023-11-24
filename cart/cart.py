2208d9fd6678921b4d96628f3457d73faa882570
mista-ai <mr.bezdar@gmail.com>
2023-11-24 17:43:41 +0300

    bugs fixed

--- cart/cart.py
+++ cart/cart.py
@@ -64,7 +64,7 @@ class Cart:
         return sum(item['quantity'] for item in self.cart.values())
 
     def get_total_price(self):
-        return sum(Decimal(item['price'] * item['quantity'] for item in self.cart.values()))
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
 
     def clear(self):
         # remove cart from session
