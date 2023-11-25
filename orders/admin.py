3417f1f39699083b5978dd72747ee24640f0c174
mista <mr.bezdar@gmail.com>
2023-11-26 01:41:23 +0600

    custom views in admin

--- orders/admin.py
+++ orders/admin.py
@@ -4,6 +4,7 @@ from django.utils.safestring import mark_safe
 import csv
 import datetime
 from django.http import HttpResponse
+from django.urls import reverse
 
 
 def export_to_csv(modeladmin, request, queryset):
@@ -30,6 +31,11 @@ def export_to_csv(modeladmin, request, queryset):
 export_to_csv.short_description = 'Export to CSV'
 
 
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
 def order_payment(obj):
     url = obj.get_stripe_url()
     if obj.stripe_id:
@@ -49,7 +55,7 @@ class OrderItemInline(admin.TabularInline):
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'address',
-                    'postal_code', 'city', 'paid', order_payment, 'created', 'updated']
+                    'postal_code', 'city', 'paid', order_payment, 'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
