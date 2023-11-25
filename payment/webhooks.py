1a88c56c39edf3063e0a1f7bbb9079bd2709288f
mista <mr.bezdar@gmail.com>
2023-11-25 23:36:58 +0600

    payment webhook

--- payment/webhooks.py
+++ payment/webhooks.py
@@ -4,12 +4,12 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from orders.models import Order
 
+
 @csrf_exempt
 def stripe_webhook(request):
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
-
     try:
         event = stripe.Webhook.construct_event(payload, sig_header, settings.STRIPE_WEBHOOK_SECRET)
     except ValueError as e:
@@ -18,16 +18,14 @@ def stripe_webhook(request):
     except stripe.error.SignatureVerificationError as e:
         # Invalid signature
         return HttpResponse(status=400)
-
     if event.type == 'checkout.session.completed':
         session = event.data.object
         if session.mode == 'payment' and session.payment_status == 'paid':
             try:
                 order = Order.objects.get(id=session.client_reference_id)
-            except:
+            except Order.DoesNotExist:
                 return HttpResponse(status=404)
             # mark order as paid
             order.paid = True
             order.save()
-
     return HttpResponse(status=200)
